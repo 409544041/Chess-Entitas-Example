43afef2fccb3fcd0d0c76b2230ba19df525d925d
DungHA <developer.kizi@gmail.com>
2019-08-17 05:11:36 +0700

    Update game config

--- Assets/Scripts/Config/GameConfigComponent.cs
+++ Assets/Scripts/Config/GameConfigComponent.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 [Config, Unique, ComponentName("GameConfig")]
 public interface IGameConfig
 {
+    Vector2Int BoardSize { get; }
     Vector2Int PawnStartPos { get; }
     Vector2Int KnightStartPos { get; }
+    GameObject PawnGO { get; }
+    GameObject KnightGO { get; }
+    float StartDelay { get; }
 }
--- Assets/Scripts/Config/GameConfigScriptable.cs
+++ Assets/Scripts/Config/GameConfigScriptable.cs
@@ -3,9 +3,17 @@
 [CreateAssetMenu]
 public class GameConfigScriptable : ScriptableObject, IGameConfig
 {
+    [SerializeField] private Vector2Int _boardSize = new Vector2Int(8, 8);
     [SerializeField] private Vector2Int _pawnStartPos = new Vector2Int(0, 0); 
-    [SerializeField] Vector2Int _knightStartPos = new Vector2Int(7, 7);
+    [SerializeField] private Vector2Int _knightStartPos = new Vector2Int(7, 7);
+    [SerializeField] private GameObject _pawnGO;
+    [SerializeField] private GameObject _knightGO;
+    [SerializeField] private float _startDelay = 1f;
 
+    public Vector2Int BoardSize => _boardSize;
     public Vector2Int PawnStartPos => _pawnStartPos;
     public Vector2Int KnightStartPos => _knightStartPos;
+    public GameObject PawnGO => _pawnGO;
+    public GameObject KnightGO => _knightGO;
+    public float StartDelay => _startDelay;
 }
